593ddf383580d6d5500b208157b70ea53296110a
kpmy <petryxa.clever@gmail.com>
2024-08-21 00:59:01 +0300

    Fix serializer and deserializer implementations

--- src/main/java/ru/practicum/lada/LadaAvroDeserializer.java
+++ src/main/java/ru/practicum/lada/LadaAvroDeserializer.java
@@ -1,32 +1,45 @@
 package ru.practicum.lada;
 
-import lombok.SneakyThrows;
+import org.apache.avro.io.BinaryDecoder;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.DecoderFactory;
+import org.apache.avro.specific.SpecificDatumReader;
 import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.common.errors.SerializationException;
 import org.apache.kafka.common.serialization.Deserializer;
 import ru.practicum.avro_example.CheckOkEventAvro;
 import ru.practicum.avro_example.ECUCheckEventAvro;
 import ru.practicum.avro_example.LightsEventAvro;
 
 public class LadaAvroDeserializer implements Deserializer<SpecificRecordBase> {
+    private final DecoderFactory decoderFactory = DecoderFactory.get();
 
-    @SneakyThrows
     @Override
     public SpecificRecordBase deserialize(String topic, byte[] bytes) {
-        SpecificRecordBase ret = null;
-        switch (topic) {
-            case LadaTopics.ECU_CHECK_REQUESTS_TOPIC:
-                ret = ECUCheckEventAvro.getDecoder().decode(bytes);
-                break;
-            case LadaTopics.ECU_CHECK_RESPONSES_TOPIC:
-                ret = CheckOkEventAvro.getDecoder().decode(bytes);
-                break;
-            case LadaTopics.LIGHTS_EVENTS_TOPIC:
-                ret = LightsEventAvro.getDecoder().decode(bytes);
-                break;
-            default:
-                throw new IllegalArgumentException("unknown generic topic " + topic);
-        }
+        try {
+            if (bytes != null) {
+                BinaryDecoder decoder = decoderFactory.binaryDecoder(bytes, null);
+                DatumReader<SpecificRecordBase> reader;
 
-        return ret;
+                switch (topic) {
+                    case LadaTopics.ECU_CHECK_REQUESTS_TOPIC:
+                        reader = new SpecificDatumReader<>(ECUCheckEventAvro.getClassSchema());
+                        break;
+                    case LadaTopics.ECU_CHECK_RESPONSES_TOPIC:
+                        reader = new SpecificDatumReader<>(CheckOkEventAvro.getClassSchema());
+                        break;
+                    case LadaTopics.LIGHTS_EVENTS_TOPIC:
+                        reader = new SpecificDatumReader<>(LightsEventAvro.getClassSchema());
+                        break;
+                    default:
+                        throw new IllegalArgumentException("Неизвестный топик: " + topic);
+                }
+
+                return reader.read(null, decoder);
+            }
+            return null;
+        } catch (Exception e) {
+            throw new SerializationException("Ошибка десереализации данных из топика [" + topic + "]", e);
+        }
     }
 }
--- src/main/java/ru/practicum/lada/LadaAvroSerializer.java
+++ src/main/java/ru/practicum/lada/LadaAvroSerializer.java
@@ -1,7 +1,11 @@
 package ru.practicum.lada;
 
-import org.apache.avro.message.BinaryMessageEncoder;
+import org.apache.avro.io.BinaryEncoder;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.io.EncoderFactory;
+import org.apache.avro.specific.SpecificDatumWriter;
 import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.common.errors.SerializationException;
 import org.apache.kafka.common.serialization.Serializer;
 
 import java.io.ByteArrayOutputStream;
@@ -9,18 +13,22 @@ import java.io.IOException;
 
 public class LadaAvroSerializer implements Serializer<SpecificRecordBase> {
 
-    private static byte[] toBytes(SpecificRecordBase event) throws IOException {
-        var outputStream = new ByteArrayOutputStream();
-        new BinaryMessageEncoder<>(event.getSpecificData(), event.getSchema()).encode(event, outputStream);
-        return outputStream.toByteArray();
-    }
+    private final EncoderFactory encoderFactory = EncoderFactory.get();
+    private BinaryEncoder encoder;
 
-    @Override
-    public byte[] serialize(String topic, SpecificRecordBase event) {
-        try {
-            return toBytes(event);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    public byte[] serialize(String topic, SpecificRecordBase data) {
+        try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+            byte[] result = null;
+            encoder = encoderFactory.binaryEncoder(out, encoder);
+            if (data != null) {
+                DatumWriter<SpecificRecordBase> writer = new SpecificDatumWriter<>(data.getSchema());
+                writer.write(data, encoder);
+                encoder.flush();
+                result = out.toByteArray();
+            }
+            return result;
+        } catch (IOException ex) {
+            throw new SerializationException("Ошибка сериализации данных для топика [" + topic + "]", ex);
         }
     }
 }
